e3541368707fa41ff7f1f0a5f589687eb321c974
joemface <joemface@yahoo.com>
2020-04-11 23:31:17 -0400

    added sound and controls

---
+++ 3_Rocket_Game/Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+        //reference to rigidbody. Rigidbody here is generics
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audio.isPlaying)
+            {
+                GetComponent<AudioSource>().Play();
+                GetComponent<AudioSource>().Play(44100);
+            }
+        }
+        else 
+        {
+            audio.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            
+            //this rotates the ship towards you..easter egg?
+            //transform.Rotate(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+
+}
